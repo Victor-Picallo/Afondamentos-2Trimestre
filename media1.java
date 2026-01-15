bfbfb571564395fae62840d8ad29918eb2d121be
Victor-Picallo <victor.p9646@gmail.com>
2026-01-15 13:37:13 +0100

    Anado media

---
+++ media1.java
@@ -0,0 +1,10 @@
+public class media1 {
+    public static void main(String[] args) {
+        
+        int[] lista = {5, 3, 8, 1, 4, 7, 9, 2, 6, 5, 3, 8, 1, 4, 7, 9, 2, 6, 5, 3, 8, 1, 4, 7, 9, 2, 6};
+
+        double media = Biblioteca.obtenerMedia(lista);
+        System.out.println("La media es: " + media);
+    }
+    
+}
